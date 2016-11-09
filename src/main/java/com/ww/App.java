bb83c0f99bd96415c32d7aead5c9b5f84b85cd93
linwenjun <linwenjun@163.com>
2016-11-09 11:28:07 +0800

    增加gradle 配置文件

--- src/main/java/com/ww/App.java
+++ src/main/java/com/ww/App.java
@@ -3,9 +3,6 @@ package com.ww;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-/**
- * Created by wjlin on 11/9/16.
- */
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
