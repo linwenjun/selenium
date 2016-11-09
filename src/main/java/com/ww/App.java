e2a0a8a94b2531a0abbe1474f889ffe3defdb192
linwenjun <linwenjun@163.com>
2016-11-09 23:13:54 +0800

    修浏览器为火狐

--- src/main/java/com/ww/App.java
+++ src/main/java/com/ww/App.java
@@ -1,12 +1,12 @@
 package com.ww;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new FirefoxDriver();
         driver.get("http://www.baidu.com");
         driver.close();
     }
